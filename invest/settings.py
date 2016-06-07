0af397191b5efa8bb282c559b189a9072c41bab6
Choo Zhi Min <choozm@gmail.com>
2016-06-07 21:05:23 +0800

    remove abolute path and use relative path in template dirs.

--- invest/settings.py
+++ invest/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'invest.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['/home/mamakstallinvestor/invest/templates',],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
